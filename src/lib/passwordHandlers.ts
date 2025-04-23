bb73da425da7f5f5ca00c699dce8a4c68c1736b1
Nzanzu Lwanzo <nzanzu.lwanzo.work@gmail.com>
2025-04-23 13:12:08 +0200

    integrate google authentication and save user in the database if they don't exist yet

--- src/lib/passwordHandlers.ts
+++ src/lib/passwordHandlers.ts
@@ -1,4 +1,5 @@
 import { hash, compare } from 'bcryptjs'
+import { generatePassword } from 'm-secure-password-generator'
 
 export const generateSecurePassword = async (raw: string) => {
     const hashedPassword = await hash(raw, 10)
@@ -8,4 +9,16 @@ export const generateSecurePassword = async (raw: string) => {
 export const doesPasswordsMatch = async (raw: string, hashed: string) => {
     const match = await compare(raw, hashed)
     return match
+}
+
+export const generateDefaultPasswordForOAuth = async () => {
+    return generatePassword({
+        avoid: "` { } [ ] ( ) ~",
+        chars: true,
+        length: 8,
+        lower: true,
+        numbr: true,
+        upper: true,
+        strct: true
+    })
 }
